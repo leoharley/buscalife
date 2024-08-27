c2300bf4ab3b100223bf5ec65351ab287b844e80
Leonardo Harley <leoharleygoncalves@gmail.com>
2024-08-27 16:37:52 -0300

    ....

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "sk-proj-g0fbWvQueDXxUgL3H4tHT3BlbkFJogxy6lh0H79jeHbba5dZ"; // Paste your API key here
+const API_KEY = ""; // Paste your API key here
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page

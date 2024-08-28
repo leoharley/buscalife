d315eeda0172d8381b0cc335606374951e8efc62
Leonardo Harley <leoharleygoncalves@gmail.com>
2024-08-28 00:19:02 -0300

    ...

--- buscalife-api/application/controllers/Api.php
+++ buscalife-api/application/controllers/Api.php
@@ -28,7 +28,7 @@ class Api extends BaseController
         if ($requestText == 'eu quero doce') {
             $aiResponse = 'to nem ai';
         } else {
-            if(preg_match('/^[A-Za-z \'-]+$/i', $requestText)){
+            if(preg_match('/(.)\\1\\1/i', $requestText)){
                 $aiResponse = 'Esse não é o nome válido de uma pessoa, informe o nome completo da pessoa que quer localizar';                
             }
             else {

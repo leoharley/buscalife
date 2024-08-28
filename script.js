636747802f032c01cff5ae8e730a30282950a48e
Leonardo Harley <leoharleygoncalves@gmail.com>
2024-08-27 21:51:40 -0300

    ....

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const loadDataFromLocalstorage = () => {
                             <img src="images/banner3.png" class="banner">
                         </div>`
 
-    document.getElementsByName('chat-input')[0].placeholder='Digite nome e sobrenome de quem você procura';                     
+    document.getElementsByName('chat-input')[0].placeholder='Qual o nome de quem você procura?';                     
 
     /*const defaultText2 = `<div class="chat incoming"><div class="chat-content">
                     <div class="chat-details">

073201ee904505accd7cb5854608532746d4d3e6
Leonardo Harley <leoharleygoncalves@gmail.com>
2024-08-27 22:50:47 -0300

    ...

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ const handleOutgoingChat = () => {
 
     const html = `<div class="chat-content">
                     <div class="chat-details">
-                        <img src="images/user.jpg" alt="user-img">
+                        <img src="images/user.png" alt="user-img">
                         <p>${userText}</p>
                     </div>
                 </div>`;
